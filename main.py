0521146f352901179b7a078499ff6a669e6a0c7b
Kevin Kipkorir <135063319+kevzkip@users.noreply.github.com>
2025-04-24 15:10:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Createid pet instance
+my_pet = Pet("Lucky")
+
+# Interact with the pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("Roll over")
+my_pet.train("Sit")
+my_pet.show_tricks()
+my_pet.get_status()
